2929ddff22afe0c76e05aa4fcc4abaf8401de931
Jewel Rana <jewel.rana.4013@gmail.com>
2023-12-03 22:04:53 +0600

    Gallery Search bar

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -5,21 +5,30 @@
 
 @section('mainLinks')
 	@parent
-	<link rel="stylesheet" href="{{ asset('css/salvattore.css') }}">	
+	<link rel="stylesheet" href="{{ asset('css/salvattore.css') }}">
 @endsection
 
 
 @section('bodyContent')
 	<div class="container-fluid mt-2 mb-4">
-		<div class="row">	
-
+        <div class="d-flex justify-content-center">
+            <form action="">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Search by Title, Date or Year">
+                    <div class="input-group-append">
+                      <span class="input-group-text" id="basic-addon2">Search</span>
+                    </div>
+                  </div>
+            </form>
+        </div>
+		<div class="row">
 			<div class="col-xl-3 col-md-4 col-sm-6 col-xs-1 mt-3 d-flex">
 				<div class="card">
 					<a href="" class="">
 						<img class="card-img-top img-thumbnail" src="{{ asset('img/gallery_img/img_4.jpg') }}" alt="Card image" style="width:100%">
 					</a>
 					<div class="card-body" style="background: #83C8F2">
-						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>						
+						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>
 						<p class="card-text text-dark"><strong>Date: </strong> 29 June, 1999</p>
 						<p class="card-text text-dark"><strong>Total Img: </strong> 27</p>
 					</div>
@@ -32,7 +41,7 @@
 						<img class="card-img-top img-thumbnail" src="{{ asset('img/gallery_img/img_4.jpg') }}" alt="Card image" style="width:100%">
 					</a>
 					<div class="card-body" style="background: #83C8F2">
-						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>						
+						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>
 						<p class="card-text text-dark"><strong>Date: </strong> 29 June, 1999</p>
 						<p class="card-text text-dark"><strong>Total Img: </strong> 27</p>
 					</div>
@@ -45,7 +54,7 @@
 						<img class="card-img-top img-thumbnail" src="{{ asset('img/gallery_img/img_4.jpg') }}" alt="Card image" style="width:100%">
 					</a>
 					<div class="card-body" style="background: #83C8F2">
-						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>						
+						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>
 						<p class="card-text text-dark"><strong>Date: </strong> 29 June, 1999</p>
 						<p class="card-text text-dark"><strong>Total Img: </strong> 27</p>
 					</div>
@@ -58,7 +67,7 @@
 						<img class="card-img-top img-thumbnail" src="{{ asset('img/gallery_img/img_4.jpg') }}" alt="Card image" style="width:100%">
 					</a>
 					<div class="card-body" style="background: #83C8F2">
-						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>						
+						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>
 						<p class="card-text text-dark"><strong>Date: </strong> 29 June, 1999</p>
 						<p class="card-text text-dark"><strong>Total Img: </strong> 27</p>
 					</div>
@@ -71,7 +80,7 @@
 						<img class="card-img-top img-thumbnail" src="{{ asset('img/gallery_img/img_4.jpg') }}" alt="Card image" style="width:100%">
 					</a>
 					<div class="card-body" style="background: #83C8F2">
-						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>						
+						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>
 						<p class="card-text text-dark"><strong>Date: </strong> 29 June, 1999</p>
 						<p class="card-text text-dark"><strong>Total Img: </strong> 27</p>
 					</div>
@@ -84,7 +93,7 @@
 						<img class="card-img-top img-thumbnail" src="{{ asset('img/gallery_img/img_4.jpg') }}" alt="Card image" style="width:100%">
 					</a>
 					<div class="card-body" style="background: #83C8F2">
-						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>						
+						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>
 						<p class="card-text text-dark"><strong>Date: </strong> 29 June, 1999</p>
 						<p class="card-text text-dark"><strong>Total Img: </strong> 27</p>
 					</div>
@@ -97,7 +106,7 @@
 						<img class="card-img-top img-thumbnail" src="{{ asset('img/gallery_img/img_4.jpg') }}" alt="Card image" style="width:100%">
 					</a>
 					<div class="card-body" style="background: #83C8F2">
-						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>						
+						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>
 						<p class="card-text text-dark"><strong>Date: </strong> 29 June, 1999</p>
 						<p class="card-text text-dark"><strong>Total Img: </strong> 27</p>
 					</div>
@@ -110,7 +119,7 @@
 						<img class="card-img-top img-thumbnail" src="{{ asset('img/gallery_img/img_4.jpg') }}" alt="Card image" style="width:100%">
 					</a>
 					<div class="card-body" style="background: #83C8F2">
-						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>						
+						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>
 						<p class="card-text text-dark"><strong>Date: </strong> 29 June, 1999</p>
 						<p class="card-text text-dark"><strong>Total Img: </strong> 27</p>
 					</div>
@@ -123,7 +132,7 @@
 						<img class="card-img-top img-thumbnail" src="{{ asset('img/gallery_img/img_4.jpg') }}" alt="Card image" style="width:100%">
 					</a>
 					<div class="card-body" style="background: #83C8F2">
-						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>						
+						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>
 						<p class="card-text text-dark"><strong>Date: </strong> 29 June, 1999</p>
 						<p class="card-text text-dark"><strong>Total Img: </strong> 27</p>
 					</div>
@@ -136,7 +145,7 @@
 						<img class="card-img-top img-thumbnail" src="{{ asset('img/gallery_img/img_4.jpg') }}" alt="Card image" style="width:100%">
 					</a>
 					<div class="card-body" style="background: #83C8F2">
-						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>						
+						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>
 						<p class="card-text text-dark"><strong>Date: </strong> 29 June, 1999</p>
 						<p class="card-text text-dark"><strong>Total Img: </strong> 27</p>
 					</div>
@@ -149,7 +158,7 @@
 						<img class="card-img-top img-thumbnail" src="{{ asset('img/gallery_img/img_4.jpg') }}" alt="Card image" style="width:100%">
 					</a>
 					<div class="card-body" style="background: #83C8F2">
-						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>						
+						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>
 						<p class="card-text text-dark"><strong>Date: </strong> 29 June, 1999</p>
 						<p class="card-text text-dark"><strong>Total Img: </strong> 27</p>
 					</div>
@@ -162,7 +171,7 @@
 						<img class="card-img-top img-thumbnail" src="{{ asset('img/gallery_img/img_4.jpg') }}" alt="Card image" style="width:100%">
 					</a>
 					<div class="card-body" style="background: #83C8F2">
-						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>						
+						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>
 						<p class="card-text text-dark"><strong>Date: </strong> 29 June, 1999</p>
 						<p class="card-text text-dark"><strong>Total Img: </strong> 27</p>
 					</div>
@@ -175,7 +184,7 @@
 						<img class="card-img-top img-thumbnail" src="{{ asset('img/gallery_img/img_4.jpg') }}" alt="Card image" style="width:100%">
 					</a>
 					<div class="card-body" style="background: #83C8F2">
-						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>						
+						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>
 						<p class="card-text text-dark"><strong>Date: </strong> 29 June, 1999</p>
 						<p class="card-text text-dark"><strong>Total Img: </strong> 27</p>
 					</div>
@@ -188,7 +197,7 @@
 						<img class="card-img-top img-thumbnail" src="{{ asset('img/gallery_img/img_4.jpg') }}" alt="Card image" style="width:100%">
 					</a>
 					<div class="card-body" style="background: #83C8F2">
-						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>						
+						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>
 						<p class="card-text text-dark"><strong>Date: </strong> 29 June, 1999</p>
 						<p class="card-text text-dark"><strong>Total Img: </strong> 27</p>
 					</div>
@@ -201,38 +210,38 @@
 						<img class="card-img-top img-thumbnail" src="{{ asset('img/gallery_img/img_4.jpg') }}" alt="Card image" style="width:100%">
 					</a>
 					<div class="card-body" style="background: #83C8F2">
-						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>						
+						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>
 						<p class="card-text text-dark"><strong>Date: </strong> 29 June, 1999</p>
 						<p class="card-text text-dark"><strong>Total Img: </strong> 27</p>
 					</div>
 				</div>
 			</div>
-			
+
 			<div class="col-xl-3 col-md-4 col-sm-6 col-xs-1 mt-3 d-flex">
 				<div class="card">
 					<a href="" class="">
 						<img class="card-img-top img-thumbnail" src="{{ asset('img/gallery_img/img_4.jpg') }}" alt="Card image" style="width:100%">
 					</a>
 					<div class="card-body" style="background: #83C8F2">
-						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>						
+						<p class="card-text text-dark"><strong>Title: </strong>Some ohn Doe is an architect and ome ohn Doe is  </p>
 						<p class="card-text text-dark"><strong>Date: </strong> 29 June, 1999</p>
 						<p class="card-text text-dark"><strong>Total Img: </strong> 27</p>
 					</div>
 				</div>
 			</div>
-			
-			
-
-			
-
-
-			
 
 
 
-			
-			
-			
+
+
+
+
+
+
+
+
+
+
 		</div>
 	</div>
 @endsection
