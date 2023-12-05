58f633a095a3dd1a355007920408c6de0f33a1e3
Jewel Office <jewel.rana.4013@gmail.com>
2023-12-05 15:37:37 +0600

    Profile key

--- resources/views/tgcLayout.blade.php
+++ resources/views/tgcLayout.blade.php
@@ -10,6 +10,8 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 		<link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        
 
 
         @section('mainLinks')
@@ -42,6 +44,7 @@
 
             <!-- Styles -->
             <link rel="stylesheet" href="{{ asset('css/tailwindcss.css') }}">            
+            <link rel="stylesheet" href="{{ asset('css/profileButton.css') }}"> 
         @show
 
     </head>
@@ -103,6 +106,7 @@
 
 
         @section('mainScripts')
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
             <!-- jquery Min JS -->
             <script src="{{ asset('js/jquery.min.js') }}"></script>
